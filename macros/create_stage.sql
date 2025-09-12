044a9bcda1df88fd3257add671a405ea882fe32f
deepz_b84@yahoo.com <deepz_b84@yahoo.com>
2025-09-12 10:39:03 +0000

    Yes

---
+++ macros/create_stage.sql
@@ -0,0 +1,7 @@
+-- macros/create_stage.sql
+{% macro create_ext_stage() %}
+    create or replace stage azure_stage
+    url='azure://stgdeviesandbox.blob.core.windows.net/devie'
+    storage_integration = my_azure_integration
+    file_format = (type = csv field_optionally_enclosed_by='"' skip_header=1);
+{% endmacro %}
