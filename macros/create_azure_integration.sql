ccfef31eb99d5378d536459ceed0d6a9368c62c8
deepz_b84@yahoo.com <deepz_b84@yahoo.com>
2025-09-12 12:41:53 +0000

    yes

--- macros/create_azure_integration.sql
+++ macros/create_azure_integration.sql
@@ -1,17 +1,13 @@
 -- macros/create_azure_integration.sql
 {% macro create_azure_integration() %}
-
 {% set sql %}
-create or replace storage integration my_azure_integration
+create or replace storage integration azure_integration
   type = external_stage
   storage_provider = azure
   enabled = true
-  azure_tenant_id = '8d620fbc-eff3-4946-8633-21f9f7dd6338'
-  storage_allowed_locations = ('azure://stgdeviesandbox.blob.core.windows.net/devie');
+  azure_tenant_id = '8d620fbc-eff3-4946-8633-21f9f7dd6338' --8d620fbc-eff3-4946-8633-21f9f7dd6338
+  storage_allowed_locations = ('azure://stgdeviesandbox.blob.core.windows.net/devie/');
 {% endset %}
-
 {{ run_query(sql) }}
-
-{% do log("Storage Integration 'my_azure_integration' created (or replaced) successfully", info=True) %}
-
+{% do log("Storage Integration 'azure_integration' created (or replaced) successfully using Managed Identity", info=True) %}
 {% endmacro %}
